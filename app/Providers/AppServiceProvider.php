1976ae20e0dd1d23ee913f34276ea2faa2ed63b1
sohrabrokhshan <sohrabrokhshan@gmail.com>
2025-10-08 11:52:40 +0430

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        if ($this->app->environment('local') && class_exists(TelescopeServiceProvider::class)) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
+
+    public function boot(): void {}
+}
